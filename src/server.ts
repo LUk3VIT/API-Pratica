df91b8a1e4284ec593d6e351ea51707b2008bc21
LUk3VIT <lucasdiaa061@gmail.com>
2025-11-06 10:39:52 -0300

    concluido

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ import { swaggerSpec } from "./config/swagger";
 import { connectDatabase } from "./config/database";
 import produtoRoutes from "./routes/produtoRoutes";
 import authRoutes from "./routes/authRoutes"; 
-import userRoutes from "./routes/UserRoutes";
-      
 
 
 const app = express();
@@ -17,7 +15,6 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/produto", produtoRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
 
 const port = 3000;
 
